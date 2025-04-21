e96a884519f73c9f5af4df02ac233dd5e81729b1
にっしー <nissi1270561@gmail.com>
2025-04-21 13:42:19 +0000

    add: Cardクラスの初期化処理を追加

--- lib/Card.rb
+++ lib/Card.rb
@@ -1,8 +1,10 @@
-
 class Card
+  # カードも絵柄も比較する可能性を考慮し、数値で保持。出力する際に、文字に変換する。
+  RANK_LABEL = { 11 => 'J',12 => 'Q', 13 => 'K', 14 => 'A', 15 => 'JOKER' }.freeze
+  SUIT_LABEL = { 1 => 'スペード',2 => 'ハート', 3 => 'ダイヤ', 4 => 'クラブ' }.freeze
+
   def initialize(suit,rank)
     @suit = suit
     @rank = rank
-    @RANK_RABLE = { 11 => 'J',12 => 'Q', 13 => 'K', 14 => 'A', 15 => 'JOKER' }
   end
 end
