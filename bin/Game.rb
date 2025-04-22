010b21e3f2ec167a90c3097bdf556865d6f1408f
nissi1278 <nissi1270561@gmail.com>
2025-04-22 20:13:27 +0900

    add: Gameクラスにメイン処理の一部を追加

--- bin/Game.rb
+++ bin/Game.rb
@@ -2,23 +2,31 @@ require_relative '../lib/Deck'
 require_relative '../lib/Player'
 
 class Game
-  def initialize
-    @player_count = 2
-  end
+  @player_count = 2
+  @player_decks = []
 
-  def create_users
-    (1..@player_count).map do |count|
-      puts "プレイヤー#{count}の名前を入力してください。"
-      player_name = gets.to_s.chomp
-      Playe.new(count,player_name)
+  class << self
+    def create_users
+      (1..@player_count).map do |count|
+        puts "プレイヤー#{count}の名前を入力してください。"
+        player_name = gets.to_s.chomp
+        Player.new(count,player_name)
+      end
+    end
+
+    def deal_cards(player_list)
+      @player_decks = Deck.deck_generate(@player_count)
+      @player_decks.each_with_index do |player_deck,i|
+        player_list[i].player_deck = player_deck
+      end
+    end
+
+    def main
+      puts '戦争を開始します。'
+      player_list = create_users
+      deal_cards(player_list)
     end
   end
-
-  def deal_cards
-    @player_decks = deck_generate(@player_count)
-  end
-
-  def main
-    puts '戦争を開始します。'
-  end
 end
+
+Game.main
